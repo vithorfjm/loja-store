b946e1d88b59841ad7b5651011e35e2e73be1f66
Vithor Félix <vithorfjm@outlook.com>
2023-08-15 00:56:40 -0300

    create Product entity

---
+++ src/main/java/br/com/vithorfjm/lojastore/model/product/Product.java
@@ -0,0 +1,60 @@
+package br.com.vithorfjm.lojastore.model.product;
+
+import br.com.vithorfjm.lojastore.model.category.Category;
+import jakarta.persistence.*;
+
+@Entity(name = "product")
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name="category_id")
+    private Category category;
+
+    public Product() {
+    }
+
+    public Product(String name, Double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
