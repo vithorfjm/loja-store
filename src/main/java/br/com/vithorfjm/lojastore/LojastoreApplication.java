33097b5947fbc195aa7876f93220aa478e8e427d
Vithor Félix <vithorfjm@outlook.com>
2023-08-15 00:31:23 -0300

    configuracoes iniciais

---
+++ src/main/java/br/com/vithorfjm/lojastore/LojastoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.vithorfjm.lojastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojastoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vithorfjm/lojastore/LojastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vithorfjm.lojastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
