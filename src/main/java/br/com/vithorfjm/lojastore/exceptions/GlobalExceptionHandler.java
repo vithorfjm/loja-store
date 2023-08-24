8230cd4f2da0fd89c5099216f818a18bb3b88b01
Vithor Félix <vithorfjm@outlook.com>
2023-08-23 23:40:53 -0300

    Add new Exception Handler for get product by id

--- src/main/java/br/com/vithorfjm/lojastore/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/com/vithorfjm/lojastore/exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,6 +15,14 @@ public class GlobalExceptionHandler {
         err.setStatus(HttpStatus.NOT_FOUND.value());
         err.setMesssage(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ExceptionDTO> argumentTypeMismatch(MethodArgumentTypeMismatchException e) {
+        ExceptionDTO err = new ExceptionDTO();
+        err.setStatus(HttpStatus.BAD_REQUEST.value());
+        err.setMesssage("Invalid argument");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
 }
